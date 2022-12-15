33c529ac4ba95c7f9616313ec91713a7f2dd230e
WhatTheMagic <robin.liljegren@edu.futuregames.nu>
2022-12-15 13:17:57 +0100

    Buttons fixed, preparing for orange platforms

--- Level Design Course Team 26/Assets/Scripts/RobinButtonDoor.cs
+++ Level Design Course Team 26/Assets/Scripts/RobinButtonDoor.cs
@@ -4,14 +4,12 @@ using UnityEngine;
 
 public class RobinButtonDoor : MonoBehaviour
 {
-    public GameObject button;
     public GameObject door;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Projectile"))
         {
-            button.SetActive(false);
             door.SetActive(false);
         }
     }
